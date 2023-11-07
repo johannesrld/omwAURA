7b5a014292ccff03ef559542f7d60ba9fd4cd9a0
johannesrld <71744831+johannesrld@users.noreply.github.com>
2023-11-07 14:39:29 +0800

    feat(lua): add inital tests of Interior music

---
+++ tlconfig.lua
@@ -0,0 +1,8 @@
+return {
+   build_dir = "scripts",
+   source_dir = "src",
+   include_dir = {
+      "/home/jrld/projects/fennel/teal_decs/openmw",
+      "home/jrld/projects/fennel/teal_decs/openmw_aux"
+   },
+}
