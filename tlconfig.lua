9760be674e91fb43fc2c759d56e1c0174036d13b
johannesrld <71744831+johannesrld@users.noreply.github.com>
2023-11-08 02:29:09 +0800

    feat: update, add eating sounds

--- tlconfig.lua
+++ tlconfig.lua
@@ -2,7 +2,9 @@ return {
    build_dir = "scripts",
    source_dir = "src",
    include_dir = {
+      "/home/jrld/teal_declarations/",
       "/home/jrld/projects/fennel/teal_decs/openmw",
-      "home/jrld/projects/fennel/teal_decs/openmw_aux"
    },
+   gen_target = "5.1",
+   get_compat = "5.1"
 }
